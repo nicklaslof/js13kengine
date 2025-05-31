272e456ee4d628518b472ace64eaf3b971e861bf
Nicklas Löf <nicklaslof76@gmail.com>
2025-05-31 17:09:46 +0200

    Moved light class to the engine

---
+++ src/engine/gameobject/light.js
@@ -0,0 +1,16 @@
+import { RenderGameobjectBehaviour } from "./behaviour/render_gameobject_behaviour.js";
+import { GameObject } from "./gameobject.js";
+import { Texture } from "../graphics/texture.js";
+
+export class Light{
+    constructor(x,y,tint,width=256,height=256){
+        this.gameObject = new GameObject(x,y,width,height);
+        this.gameObject.tint = tint;
+        this.gameObject.renderPassLight = true;
+        
+        let render_gameobject_behaviour = new RenderGameobjectBehaviour(Texture.LIGHTX,Texture.LIGHTY,Texture.LIGHTWH,Texture.LIGHTWH);
+        render_gameobject_behaviour.tint = 0xffffffff;
+        this.gameObject.behaviours.push(render_gameobject_behaviour);
+        GameObject.addGameObject(this.gameObject);
+    }
+}
--- src/game/screen/gamescreen.js
+++ src/game/screen/gamescreen.js
@@ -2,21 +2,38 @@ import { Engine } from "../../engine/engine.js";
 import { GameObject } from "../../engine/gameobject/gameobject.js";
 import { Screen } from "../../engine/screen/screen.js";
 import { Ball } from "../entity/ball.js";
-import { Light } from "../entity/light.js";
+import { Light } from "../../engine/gameobject/light.js";
 import { Level } from "../level/level.js";
+import { Behaviour } from "../../engine/gameobject/behaviour/behaviour.js";
 
 export class GameScreen extends Screen{
     constructor(){
         super(0,0,0,0.2,0.2,0.2);
 
-        for (let i = 0; i < 6000; i++) {    
+        /*for (let i = 0; i < 6000; i++) {    
             var ball = new Ball(Math.random()*W,Math.random()*H);
             ball.gameObject.tint = Math.random()*4294967295
             GameObject.addGameObject(ball.gameObject); 
-        }
+        }*/
 
-        for (let i =0; i < 128; i++){
-            new Light(Engine.getRandom(0,W),Engine.getRandom(0,H),Engine.getRandom(0x000000ff,0xffffffff));
+        for (let i =0; i < 64; i++){
+            let l = new Light(Engine.getRandom(0,W),Engine.getRandom(0,H),Engine.getRandom(0x000000ff,0xffffffff),256,256);
+            let b = new Behaviour();
+            b.tick = (gameObject, deltaTime) => {
+                if (gameObject.counter == null) gameObject.counter = Engine.getRandom(0,3600);
+                if (gameObject.orgX == null) gameObject.orgX = gameObject.x;
+                if (gameObject.orgY == null) gameObject.orgY = gameObject.y;
+                
+                gameObject.counter +=deltaTime;
+
+                let s = Math.sin(gameObject.counter/300);
+                let c = Math.cos(gameObject.counter/250);
+
+                gameObject.x = gameObject.orgX + (s*25);
+                gameObject.y = gameObject.orgY + (c*50);
+            }
+
+            l.gameObject.behaviours.push(b);
         }
         
 
